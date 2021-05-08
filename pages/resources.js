103a84454baf713060fd0ee56076afd3afdf20c0
Tom Barnes <barnes.thomas.c@gmail.com>
2021-05-07 22:13:30 -0400

    fixing static errors

--- pages/resources.js
+++ pages/resources.js
@@ -1,16 +1,11 @@
 import Head from 'next/head';
-import bucket from "../lib/cosmic";
 
-export default function Team({team}) {
-
+export default function Resources() {
 	return (
 		<>
 			<Head>
 		        <title>#MapTheMovement | Resources</title>
 		        <link rel="icon" href="/frame.ico" />
-		        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.4.0/dist/leaflet.css"
-		          integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA=="
-		          crossOrigin=""/>
 	      	</Head>
 	      	<main>
 	      		<div className='contentContainer'>
@@ -20,20 +15,4 @@ export default function Team({team}) {
 	      	</main>
       	</>
 	)
-}
-
-export async function getStaticProps(ctx) {
-  const data = await bucket.getObjects({
-    query: {
-    	type: 'team-members'
-    },
-    sort: 'created_at',
-    props: 'slug,title,metadata'
-  })
-  const team = await data.objects
-  return {
-    props: {
-    	team: team
-    }
-  }
 }

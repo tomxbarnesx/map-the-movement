aac0bba88166e7fb274509f687f149e0525f719a
Tom Barnes <barnes.thomas.c@gmail.com>
2021-05-10 13:01:41 -0400

    updated env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    COSMIC_SLUG: process.env.COSMIC_SLUG
+    COSMIC_SLUG: process.env.COSMIC_SLUG,
     COSMIC_READ_KEY: process.env.COSMIC_READ_KEY, // Pass through env variables
   },
 }

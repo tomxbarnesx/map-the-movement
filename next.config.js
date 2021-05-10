ce1f78ddde9ad94da0416b41a7d108296c5522b0
Tom Barnes <barnes.thomas.c@gmail.com>
2021-05-10 13:10:35 -0400

    updated server runtime config

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ module.exports = {
     COSMIC_SLUG: process.env.COSMIC_SLUG,
     COSMIC_READ_KEY: process.env.COSMIC_READ_KEY, // Pass through env variables
   },
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    COSMIC_SLUG: process.env.COSMIC_SLUG,
+    COSMIC_READ_KEY: process.env.COSMIC_READ_KEY, // Pass through env variables
+  },
 }
